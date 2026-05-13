4905f06fb984dcab5d696c74f33581faab246289
unemotioned <blackeagle10@icloud.com>
2026-05-13 14:37:31 +0900

    feat(build.sh): backup the previously built firmwares to output/bak/

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 # NOTE: Change the keyboard names and controller type from here.
 shield_left='corne_left nice_view_adapter nice_view'
 shield_right='corne_right nice_view_adapter nice_view'
-board='nice_nano_v2'
+controller='nice_nano_v2'
 
 venv_dir="$HOME/venv/zmk"
 
@@ -44,15 +44,32 @@ build_target() {
     -DCMAKE_POLICY_VERSION_MINIMUM=3.5
 }
 
-build_target build/left "$shield_left" "$board"
-build_target build/right "$shield_right" "$board"
+build_target build/left "$shield_left" "$controller"
+build_target build/right "$shield_right" "$controller"
 # NOTE: Uncomment the following line to build the "settings_reset.uf2" file.
 # build_target build/settings_reset settings_reset "$controller"
 
-mkdir -p output
-cp build/left/zephyr/zmk.uf2 output/"${shield_left}".uf2
-cp build/right/zephyr/zmk.uf2 output/"${shield_right}".uf2
-[ -f build/settings_reset/zephyr/zmk.uf2 ] && cp build/settings_reset/zephyr/zmk.uf2 output/settings_reset.uf2
+mkdir -p output/bak
+
+# backup to bak/
+[ -f output/"$shield_left".uf2 ] &&
+  mv output/"$shield_left".uf2 output/bak
+
+[ -f output/"$shield_right".uf2 ] &&
+  mv output/"$shield_right".uf2 output/bak
+
+[ -f output/settings_reset.uf2 ] &&
+  mv output/settings_reset.uf2 output/bak
+
+# copy the built uf2 to output/
+[ -f build/left/zephyr/zmk.uf2 ] &&
+  cp build/left/zephyr/zmk.uf2 output/"$shield_left".uf2
+
+[ -f build/right/zephyr/zmk.uf2 ] &&
+  cp build/right/zephyr/zmk.uf2 output/"$shield_right".uf2
+
+[ -f build/settings_reset/zephyr/zmk.uf2 ] &&
+  cp build/settings_reset/zephyr/zmk.uf2 output/settings_reset.uf2
 
 echo -e "\n----------------------------------------------"
 echo -e "\n Build done. (took ${SECONDS}s)"
@@ -63,4 +80,3 @@ read -rp 'Open output directory with finder? [y/N]: ' answer
 if [[ "${answer,,}" == 'y' ]]; then
   open ./output
 fi
-
