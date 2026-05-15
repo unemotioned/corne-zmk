08f218ef3cfb1effd319f0eb43354d1c7b87eda7
unemotioned <blackeagle10@icloud.com>
2026-05-15 12:12:51 +0900

    Refactor: control building reset file with &build_reset bool var open ouput/ by default after build remove -p always from $ west build remove unsetting flags add .editorconfig

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# NOTE: Change the keyboard names and controller type from here.
+# NOTE: Change the keyboard names, controller type and etc from here.
 shield_left='corne_left nice_view_adapter nice_view'
 shield_right='corne_right nice_view_adapter nice_view'
 controller='nice_nano_v2'
-
+build_reset=false
 venv_dir="$HOME/venv/zmk"
 
 # absolute path to script's directory not where you ran it
@@ -18,10 +18,6 @@ source "$venv_dir/bin/activate"
 # source Zephyr SDK env var
 source "$SCRIPT_DIR/zephyr_env.sh"
 
-# remove Homebrew injected flags only for this script
-unset CPPFLAGS
-unset LDFLAGS
-
 # make sure west to use repo dir
 cd "$ROOT_DIR"
 
@@ -29,54 +25,57 @@ cd "$ROOT_DIR"
 SECONDS=0
 
 build_target() {
-  local build_dir="$1"
-  local shield="$2"
-  local controller="$3"
+    local build_dir="$1"
+    local shield="$2"
+    local controller="$3"
 
-  west build \
-    -d "$build_dir" \
-    -p always \
-    -b "$controller" \
-    -s zmk/app \
-    -- \
-    -DSHIELD="$shield" \
-    -DZMK_CONFIG="$ROOT_DIR/config" \
-    -DCMAKE_POLICY_VERSION_MINIMUM=3.5
+    # NOTE: Add the following option in "$ west build" before the "--" to build cleanly
+    # -p always \
+    west build \
+        -d "$build_dir" \
+        -b "$controller" \
+        -s zmk/app \
+        -- \
+        -DSHIELD="$shield" \
+        -DZMK_CONFIG="$ROOT_DIR/config" \
+        -DCMAKE_POLICY_VERSION_MINIMUM=3.5
 }
 
 build_target build/left "$shield_left" "$controller"
 build_target build/right "$shield_right" "$controller"
-# NOTE: Uncomment the following line to build the "settings_reset.uf2" file.
-# build_target build/settings_reset settings_reset "$controller"
+
+if [ "$build_reset" = true ]; then
+    build_target build/settings_reset settings_reset "$controller"
+fi
 
 mkdir -p output/bak
 
 # backup to bak/
 [ -f output/"$shield_left".uf2 ] &&
-  mv output/"$shield_left".uf2 output/bak
+    mv output/"$shield_left".uf2 output/bak
 
 [ -f output/"$shield_right".uf2 ] &&
-  mv output/"$shield_right".uf2 output/bak
+    mv output/"$shield_right".uf2 output/bak
 
 [ -f output/settings_reset.uf2 ] &&
-  mv output/settings_reset.uf2 output/bak
+    mv output/settings_reset.uf2 output/bak
 
 # copy the built uf2 to output/
 [ -f build/left/zephyr/zmk.uf2 ] &&
-  cp build/left/zephyr/zmk.uf2 output/"$shield_left".uf2
+    cp build/left/zephyr/zmk.uf2 output/"$shield_left".uf2
 
 [ -f build/right/zephyr/zmk.uf2 ] &&
-  cp build/right/zephyr/zmk.uf2 output/"$shield_right".uf2
+    cp build/right/zephyr/zmk.uf2 output/"$shield_right".uf2
 
-[ -f build/settings_reset/zephyr/zmk.uf2 ] &&
-  cp build/settings_reset/zephyr/zmk.uf2 output/settings_reset.uf2
+[[ "$build_reset" = true && -f build/settings_reset/zephyr/zmk.uf2 ]] &&
+    cp build/settings_reset/zephyr/zmk.uf2 output/settings_reset.uf2
 
 echo -e "\n----------------------------------------------"
 echo -e "\n Build done. (took ${SECONDS}s)"
 echo -e "\n uf2 files are copied to the output directory."
 echo -e "\n----------------------------------------------\n"
 
-read -rp 'Open output directory with finder? [y/N]: ' answer
-if [[ "${answer,,}" == 'y' ]]; then
-  open ./output
+read -rp 'Open output directory with finder? [Y/n]: ' answer
+if [[ -z "${answer,,}" || "${answer,,}" == 'y' ]]; then
+    open ./output
 fi
